db5fd2017d1061ada10db0e95b2f6a2bba6c7d92
elimango <liveeha3@gmail.com>
2025-06-17 10:24:00 -0400

    send appropriate scripts to their new branches

--- mango-recurse.py
+++ mango-recurse.py
@@ -2,52 +2,36 @@ import c4d
 
 doc = c4d.documents.GetActiveDocument()
 
-def getAllRenderData(op):
-    settings = []
-    while op:
-        settings.append(op.GetName())
-        settings += getAllRenderData(op.GetDown())  # Recurse into children
-        op = op.GetNext()
-    return settings
-
-def getAllMaterials(doc):
-    materials = []
+def recurse(doc, data):
+    if not data:
+        data = []
     while doc:
-        materials.append(doc)
+        data.append(doc.GetName())
+        data += recurse(doc.GetDown())  # Recurse into children
         doc = doc.GetNext()
-    return materials
-
-def getAllObjects(doc):
-    # recursively collects all objects in the hierarchy starting from op
-    objects = []
-    while doc:
-        objects.append(doc)
-        objects += getAllObjects(doc.GetDown())  # Recurse into children
-        doc = doc.GetNext()  # Move to next sibling
-    return objects
+    return data
 
 def main(): 
     if doc is None:
         print("No active document.")
 
     print("\nMaterials:")
-    collectmats = getAllMaterials(doc.GetFirstMaterial())    
+    collectmats = recurse(doc.GetFirstMaterial())    
     for mat in collectmats:
         print(f"  {mat.GetName()}")
     print(f"Found {len(collectmats)}")           
 
     print("\nObjects:")   
-    collectobj = getAllObjects(doc.GetFirstObject())
+    collectobj = recurse(doc.GetFirstObject())
     for obj in collectobj:
         print(f"  {obj.GetName()}")    
     print(f"Found {len(collectobj)}")   
 
     print("\nRender Settings:")   
-    collectrd = getAllRenderData(doc.GetFirstObject())
+    collectrd = recurse(doc.GetFirstRenderData())
     for rd in collectrd:
         print(f"  {rd.GetName()}")    
     print(f"Found {len(collectrd)}")      
-
     c4d.EventAdd()
 
 if __name__ == '__main__':
